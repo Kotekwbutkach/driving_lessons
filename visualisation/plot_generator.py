8c037112e4adb343011593bcb4c91f04450bcac8
Kotekwbutkach <agencja.anm@gmail.com>
2023-05-28 14:43:56 +0200

    Reworked PlotGenerator into instance class

--- visualisation/plot_generator.py
+++ visualisation/plot_generator.py
@@ -7,14 +7,27 @@ from visualisation import from_id
 
 
 class PlotGenerator:
-    @staticmethod
-    def plot_vehicle_data(road: Road, name: str, start: int = 0, end: Union[int, None] = None):
-        end = road.time_step if end is None else min(end, road.time_step)
+    road: Road
+    name: str
+
+    def __init__(self,
+                 road: Road,
+                 name: str = "plot",
+                 start: int = 0,
+                 end: Union[int, None] = None):
+        self.road = road
+        self.name = name
+        self.start = start
+        self.end = self.road.time_step if end is None else min(end, self.road.time_step)
+
+    def plot_vehicle_data(self):
         value_types = [(0, "position"), (1, "velocity"), (2, "acceleration")]
 
         for value_id, value_name in value_types:
-            for vehicle_id in range(road.number_of_vehicles):
-                plt.plot(list(range(start, end)), road.road_data[start:end, vehicle_id][:, value_id],
-                         ",", color=from_id(vehicle_id, sat=0.75))
-            plt.savefig(f"graphs/{name}_{value_name}.png", format="png")
+            for vehicle_id in range(self.road.number_of_vehicles):
+                plt.plot(list(range(self.start, self.end)),
+                         self.road.road_data[self.start:self.end, vehicle_id][:, value_id],
+                         ",",
+                         color=from_id(vehicle_id, sat=0.75))
+            plt.savefig(f"graphs/{self.name}_{value_name}.png", format="png")
             plt.clf()
